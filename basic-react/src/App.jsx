250267b6ed9a358676432ca5b62322218506a2ac
seawachiMac <seawachi27@gmail.com>
2025-06-28 15:36:50 +0700

    reducer backup

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState ,useReducer} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Transection from './components/Transection'
@@ -6,33 +6,62 @@ import FormComponent from './components/FormComponent';
 import './App.css'
 import DataContext from './data/DataContext';
 import ReportComponent from './components/Reportcomponent';
+import { element } from 'prop-types';
 
 function App() {
   const design = {color:'red',textAlign:'center',fontSize:'1.5rem'};
   const initdata = [
-    {id:1, title: 'Valid', amount: 100 },
-    {id:2, title: "123" ,amount: 500 },
-    {id:3, title: 'Text', amount: 700},     
+    {id:1, title: 'test1', amount: 100 },
+    {id:2, title: "test2" ,amount: 500 },
+    {id:3, title: 'Text3', amount: -700},     
+    {id:4, title: 'Text4', amount: 7100},     
+    {id:5, title: 'Text5', amount: -500},     
   ];
   const [items,setItems] = useState([])
+  const [reportIncome,setReportIncome] = useState(0)
+  const [reportExpense,setReporExpense] = useState(0)
+
   const onAddNewItem = (newItem)=>{
     setItems((prevItem) =>{
       return [newItem,...prevItem]
     })
     console.log('new item formcom.',newItem)
   }
+  useEffect(()=>{
+    const amounts = items.map(items => items.amount)
+    const income = amounts.filter(element=>element>0).reduce((total,element)=>total+=element,0)
+    const expense = (amounts.filter(element=>element<0).reduce((total,element)=>total+=element,0))*-1
+    setReporExpense(expense)
+    setReportIncome(income)
+  },[items,reportExpense,reportIncome])
+  //reducer state
+  const [showReport,setshowReport] = useState(0)
+  const reducer = (state,action)=>{
+    switch(action.type){
+      case "SHOW": 
+        return setshowReport(true)
+      case "HIDE":
+        return setshowReport(false)
+    }
+  }
+  const [result,dispatch] = useReducer(reducer,showReport)
   return (
       <DataContext.Provider value={
         {
-          income : 50000,
-          expense: -8000
+          income : reportIncome,
+          expense: reportExpense
         }
       }>
         <div className='container'>
             <h1 style={design}>แอพบัญชี</h1>
-            <ReportComponent/>
+            {showReport && <ReportComponent/>}
+            
             <FormComponent onAddItem={onAddNewItem}/>
             <Transection items = {items}/>
+
+            <h1>{result}</h1>
+            <button onClick={()=>dispatch({type:"SHOW"})}>Show</button>
+            <button onClick={()=>dispatch({type:"HIDE"})}>Hide</button>
         </div>
       </DataContext.Provider>
     );
--- basic-react/src/components/Reportcomponent.jsx
+++ basic-react/src/components/Reportcomponent.jsx
@@ -1,11 +1,23 @@
 import { useContext } from "react"
 import DataContext from "../data/DataContext"
+import "./ReportComponent.css"
 const ReportComponent=()=>{
     const {income,expense} = useContext(DataContext)
     return(
         <div>
-            <p>income : {income}</p>
-            <p>expense : {expense}</p>
+            <h4>ยอดคงเหลือ(บาท)</h4>
+            <h1>฿{income-expense}</h1>
+            <div className="report-container">
+                <div>
+                    <h4>ยอกรายรัย</h4>
+                    <p className="report plus">฿{income}</p>
+                </div>
+
+                <div>
+                    <h4>ยอดรายจ่าย</h4>
+                    <p className="report minus">฿{expense}</p>
+                </div>
+            </div>
         </div>
     )
 }
